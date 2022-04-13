e818f717c40749dbae03f685d99e9c134811eaf8
ShamilUmarov <umarovsamil209@gmail.com>
2022-04-13 16:14:58 +0600

    test 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import "./Header.css";
-// import headerImage from "../../assets/header5.jpg";
+import headerImage from "../../assets/header5.jpg";
 
 function Header() {
-  return <header className="Header" ></header>;
+  return <header className="Header" >
+    <img src={headerImage} alt="image" />
+  </header>;
 }
 
 export default Header;
